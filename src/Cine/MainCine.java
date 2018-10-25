1de2d36423e2a587c8102f76b0138d778769137d
Edgar Araya <edgar.araya1801@alumnos.ubiobio.cl>
2018-10-25 19:01:40 -0300

    Initial commit.

---
+++ src/Cine/MainCine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cine;
+
+/**
+ *
+ * @author Edgar-pc
+ */
+public class MainCine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
