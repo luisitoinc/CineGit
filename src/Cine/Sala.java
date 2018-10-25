af7babdc0c6527d453c1e67fe5dc9a228731a07a
Edgar Araya <edgar.araya1801@alumnos.ubiobio.cl>
2018-10-25 19:15:15 -0300

    2 commit.

--- src/Cine/Sala.java
+++ src/Cine/Sala.java
@@ -5,10 +5,20 @@
  */
 package Cine;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Edgar-pc
  */
 public class Sala {
+    
+    private byte totalFilas;
+    private byte totalColumnas;
+    private Asiento[][] asientos;
+    private short valorEntrada;
+    private String nombre;
+    private static ArrayList<Character> nombreFilas;
+    
     
 }
